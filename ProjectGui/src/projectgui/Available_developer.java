85870a42764e7950114ab8e8d55661210dc1ede2
DaliaAdel <34524724+DaliaAdel@users.noreply.github.com>
2018-05-07 19:49:22 +0200

    Add files via upload

--- ProjectGui/src/projectgui/Available_developer.java
+++ ProjectGui/src/projectgui/Available_developer.java
@@ -18,20 +18,19 @@ import java.util.ArrayList;
  */
 public class Available_developer  {
 
-    
+    StringAccessor access=new StringAccessor();
+    FileFacade facade =new  FileFacade();
    
     public ArrayList get_availableMember(ArrayList<String> Lines) throws FileNotFoundException, IOException
     {
          ArrayList<String>T_MR=new ArrayList<>();
         ArrayList<String>T_M=new ArrayList<>();
+        ArrayList<String>res=new ArrayList<>();
          ArrayList<String>result=new ArrayList<>();
-        File file =new File("C:\\Users\\lenovoo\\Desktop\\MemberTaskRelation.txt");
-          BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\lenovoo\\Desktop\\MemberTaskRelation.txt"));  
-          String str;   
-          while((str=br.readLine()) !=null)
-          {         
-              T_MR.add(str);
-          }
+       
+    
+        String s=access.FileTaskMemberRelationPath;
+       T_MR =facade.getfile(s);
         for(int i=0 ;i<T_MR.size();i=i+2)
         {  
             T_M.add(T_MR.get(i)+"\r\n");
@@ -39,7 +38,6 @@ public class Available_developer  {
         for(int j=0 ;j<Lines.size();j++)
         {
              String line=Lines.get(j).trim();
-             //String[] datarow =line.split(",");
              int count=0;
             for(int i=0 ;i<T_M.size();i++)
             {    
